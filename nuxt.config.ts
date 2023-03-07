911c6862b8a46834ba9cfa6bb6f933f63fea81c3
marg0029 <chaymae.margoum@etudiant.univ-reims.fr>
2023-03-07 21:12:56 +0100

    new

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  app: {
-    baseURL: "/iptv-app/", // baseURL: '/<repository>/'
+  experimental: {
+    payloadExtraction: false,
   },
 });
